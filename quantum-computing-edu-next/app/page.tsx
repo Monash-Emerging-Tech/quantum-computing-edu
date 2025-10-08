ccd2d8e46a07b11b90bf5e51a52d6f39f4611624
Anthony Wilson <awilsonprogramming@gmail.com>
2025-10-08 11:31:49 +1100

    Add sections below circuit
    - Added sections for explanations, proofs, sources, and other information.

--- quantum-computing-edu-next/app/page.tsx
+++ quantum-computing-edu-next/app/page.tsx
@@ -20,11 +20,67 @@ export default function Home() {
  */
 const CircuitPage = () =>
   <div id="circuit-page-container" className={styles["circuit-page-container"]}>
-    <div id="page-header"  className={styles["page-header"]}>
+    <h1 id="page-header"  className={styles["page-header"]}>
       Interactive Quantum Circuits
-    </div>
-    <div id="circuit-header" className={styles["circuit-header"]}>
+    </h1>
+    
+    <h2 id="circuit-header" className={styles["circuit-header"]}>
       Harrow-Hassidim-Lloyd Algorithm
-    </div>
+    </h2>
+    
     <Circuit />
+    
+    <div id="circuit-information-container">
+      <section className={styles["information-section"]}>
+        <h3 className={styles["h3"]}>Algorithm Overview</h3>
+        
+        <p>
+          The Harrow-Hassidim-Lloyd Algorithm inverts a Hermitian matrix, <code>A</code>,
+          and creates a quantum state corresponding to the solution of <code>Ax = b</code>,
+          where A and b are known.
+        </p>
+        
+      </section>
+      
+      <section className={styles["information-section"]}>
+        <h3 className={styles["h3"]}>Circuit Explanation</h3>
+        
+        <p>
+          The circuit requires oracular access to the Hermitian matrix <code>A</code>.{" "}
+          For current implementations, the matrix is converted to a unitary operator,{" "}
+          <code>U</code>, which is used during construction of the circuit.
+          
+          After construction of the main circuit, the qubits in the <code>state</code>{" "}
+          register are initialised using the input vector <code>b</code>.
+          All other qubits are initialised to <code>|0⟩</code>.
+        </p>
+        
+      </section>
+      
+      <section className={styles["information-section"]}>
+        <h3 className={styles["h3"]}>Mathematical Proofs</h3>
+      </section>
+      
+      <section className={styles["information-section"]}>
+        <h3 className={styles["h3"]}>Literature</h3>
+        
+        <p>
+          Quantum algorithm for linear systems of equations
+          <br />
+          [citation]
+        </p>
+      </section>
+      
+      <section className={styles["information-section"]}>
+        <h3 className={styles["h3"]}>Use Cases</h3>
+      </section>
+      
+      <section className={styles["information-section"]}>
+        <h3 className={styles["h3"]}>Extra Information</h3>
+        
+        <h4>Theoretical problems</h4>
+        
+        <h4>Implementation problems</h4>
+      </section>
+    </div>
   </div>
