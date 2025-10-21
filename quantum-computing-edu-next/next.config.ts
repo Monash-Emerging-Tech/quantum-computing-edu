702084fd011aea8bef80ed84e3cd32a49f3c2a4e
Anthony Wilson <awilsonprogramming@gmail.com>
2025-10-21 13:34:29 +1100

    Add support for MDX with MathJax
    - Added MDX packages and configuration according to Next JS's guide (https://nextjs.org/docs/app/guides/mdx).
    - Added remark-math and rehype-mathjax plugins.

--- quantum-computing-edu-next/next.config.ts
+++ quantum-computing-edu-next/next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx';
+
+//import remarkMath from 'remark-math'
+//import rehypeMathJax from 'rehype-mathjax'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+  options: {
+    remarkPlugins: [
+      'remark-math',
+      //remarkMath
+    ],
+    rehypePlugins: [
+      'rehype-mathjax',
+      //rehypeMathJax
+    ],
+  },
+});
+
+export default withMDX(nextConfig);
