d73059a2f8f736a8bd777c294b1cf058e15ada2f
Anthony Wilson <awilsonprogramming@gmail.com>
2026-02-04 15:29:12 +1100

    Add matrix and descriptions to gate popups
    - Added dynamic MDX loading in the GateInfoBubble component to load the popup-formatted description of the gate.
    - Added the UnitaryMatrixVisual to the GateInfoBubble popover as well.

--- quantum-computing-edu-next/app/components/info_bubbles.tsx
+++ quantum-computing-edu-next/app/components/info_bubbles.tsx
@@ -5,13 +5,19 @@
  * Components for the the information bubbles that pop up upon hover.
  */
 
-import { Operation } from "../circuit-data/circuit-parsing";
+import fs from "fs";
+
+import Link from "next/link";
+
+import { Operation } from '@/app/circuit-data/circuit-parsing';
+
+import UnitaryMatrixVisual from "@/app/components/matrix";
 
 import {
   PopoverContent,
-  PopoverDescription,
+  //PopoverDescription,
   PopoverHeading,
-  PopoverClose
+  //PopoverClose
 } from "./popover";
 
 import styles from "./info-bubbles.module.css";
@@ -22,10 +28,41 @@ import styles from "./info-bubbles.module.css";
  */
 const GateInfoBubble = ({operation}: {operation: Operation}) => {
   return <PopoverContent className={styles["gate-info-bubble"]}>
-    <PopoverHeading>Gate: {operation.gate.full_name}</PopoverHeading>
-    <PopoverDescription>Description of this gate goes here.</PopoverDescription>
-    {/*<PopoverClose>Close</PopoverClose>*/}
+    <PopoverHeading>
+      <Link href={"/gates/"+operation.gate.gate_id}>
+        Gate: {operation.gate.full_name}
+      </Link>
+    </PopoverHeading>
+    <GatePopoverDescription operation={operation} />
   </PopoverContent>;
+}
+
+/**
+ * Create the gate information page
+ * @returns JSX content for the gate page
+ */
+async function GatePopoverDescription({operation}: {operation: Operation}) {
+  
+  // Attempt to import the gate documentation from the relevant markdown file, if it is defined & it exists
+  let MarkdownPage = () => <></>;
+  if (
+    operation.gate.documentation_file !== undefined &&
+    operation.gate.documentation_file !== "" &&
+    fs.existsSync(`${process.cwd()}/app/circuit-data/popup-descriptions/${operation.gate.documentation_file}`)
+  ) {
+    const { default: MarkdownPage_import } = await import(`@/app/circuit-data/popup-descriptions/${operation.gate.documentation_file}`);
+    MarkdownPage = MarkdownPage_import;
+  }
+  
+  return (
+    <div>
+      { operation.gate.unitary ? <UnitaryMatrixVisual matrix={operation.gate.unitary} /> : <></> }
+      
+      <div id="gate-information-container" className={styles["gate-information-container"]}>
+        <MarkdownPage />
+      </div>
+    </div>
+  )
 }
 
 export default GateInfoBubble;
