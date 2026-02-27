13992fa6a243877972ec38d8fc9910951b438ad9
Anthony Wilson <awilsonprogramming@gmail.com>
2026-02-27 19:47:41 +1100

    Refactor OperationComponent code
    - Moved actual gate rectangle generation into a new GateComponent
      component.
      - This component handles size, but not position.
    - Position is handled by the OperationComponent wrapper.

--- quantum-computing-edu-next/app/components/gate.tsx
+++ quantum-computing-edu-next/app/components/gate.tsx
@@ -17,25 +17,23 @@ import { calculateGateDimensions } from "./circuit-functions";
 
 import styles from "./circuit.module.css";
 
-// Import info bubbles
-import GateInfoBubble from "./info_bubbles";
-
 import {
   Popover,
   PopoverTrigger,
-  PopoverContent,
-  PopoverDescription,
-  PopoverHeading,
-  PopoverClose
+  //PopoverContent,
+  //PopoverDescription,
+  //PopoverHeading,
+  //PopoverClose
 } from "./popover";
 
 
 
 /**
- * Create quantum gate
- * @param gate All gate information
+ * Create quantum gate (operation) visual in the context of a circuit
+ * @param operation All gate information
  * @param qubitPositions Vertical qubit arrangement
  * @param timePosition Current horizontal position in the circuit
+ * @param info_bubble_child Child information bubble element
  * @returns JSX element
  */
 const OperationComponent = (
@@ -83,26 +81,73 @@ const OperationComponent = (
   
   const [gateWidth, gateHeight] = calculateGateDimensions(operation, qubitPositions);
   
-  return <Popover>
-    <PopoverTrigger asChild={true}>
-      <div
-        className={styles["circuit-gate"]}
-        style={{
-          top: (upperQubitPos+0.5)*lineSeparation*2 + "em",
-          left: (timePosition * gateMargin * 3 + 1) + "em",
-          height: ((gateHeight - 1) * lineSeparation * 2 + gateBaseSize) + "em",
-          width: (gateWidth * gateBaseSize + gateMargin * (gateWidth-1)) + "em",
-          lineHeight: ((gateHeight - 1) * lineSeparation * 2 + gateBaseSize) + "em",
-          background: operation.gate.color,
-        }}
-      >
-        {operation.gate.gate_id != "barrier" ? operation.gate.display_name : <></>}
-        {operation.exponent > 1 ? (<sup>{(operation.inverse ? "-" : "") + operation.exponent.toString()}</sup>) : (operation.inverse ? (<sup>†</sup>) : <></>)}
-      </div>
-    </PopoverTrigger>
-    {info_bubble_child}
-  </Popover>
+  return <div
+    className={styles["circuit-operation-container"]}
+    style={{
+      top: (upperQubitPos+0.5)*lineSeparation*2 + "em",
+      left: (timePosition * gateMargin * 3 + 1) + "em",
+    }}
+  >
+    <GateComponent
+      operation={operation}
+      lineSeparation={lineSeparation}
+      gateBaseSize={gateBaseSize}
+      gateMargin={gateMargin}
+      gateWidth={gateWidth}
+      gateHeight={gateHeight}
+      info_bubble_child={info_bubble_child}
+    />
+  </div>
 }
+
+
+
+/**
+ * Create quantum gate visual independent of the circuit context
+ * @param operation All gate information
+ * @param lineSeparation Distance between lines in the circuit defined by CSS
+ * @param gateBaseSize Base gate size defined by CSS
+ * @param gateMargin Margin between gates defined by CSS
+ * @param gateWidth Relative width of the gate
+ * @param gateHeight Height of the gate (in qubits)
+ * @param info_bubble_child Child information bubble element
+ * @returns JSX element
+ */
+const GateComponent = (
+  {
+    operation,
+    lineSeparation,
+    gateBaseSize,
+    gateMargin,
+    gateWidth,
+    gateHeight,
+    info_bubble_child
+  }: {
+    operation: Operation,
+    lineSeparation: number,
+    gateBaseSize: number,
+    gateMargin: number,
+    gateWidth: number,
+    gateHeight: number,
+    info_bubble_child: React.ReactNode
+  }
+) => <Popover>
+  <PopoverTrigger asChild={true}>
+    <div
+      className={styles["circuit-gate"]}
+      style={{
+        height: ((gateHeight - 1) * lineSeparation * 2 + gateBaseSize) + "em",
+        width: (gateWidth * gateBaseSize + gateMargin * (gateWidth-1)) + "em",
+        lineHeight: ((gateHeight - 1) * lineSeparation * 2 + gateBaseSize) + "em",
+        background: operation.gate.color,
+      }}
+    >
+      {operation.gate.gate_id != "barrier" ? operation.gate.display_name : <></>}
+      {operation.exponent > 1 ? (<sup>{(operation.inverse ? "-" : "") + operation.exponent.toString()}</sup>) : (operation.inverse ? (<sup>†</sup>) : <></>)}
+    </div>
+  </PopoverTrigger>
+  {info_bubble_child}
+</Popover>;
 
 
 
