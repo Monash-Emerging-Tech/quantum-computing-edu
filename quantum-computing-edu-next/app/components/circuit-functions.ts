4f38d5b6df891194db8806e5d6a1637d2a84dc7a
Anthony Wilson <awilsonprogramming@gmail.com>
2026-02-27 22:12:54 +1100

    Move operation qubit span calculations into circuit-functions

--- quantum-computing-edu-next/app/components/circuit-functions.ts
+++ quantum-computing-edu-next/app/components/circuit-functions.ts
@@ -13,7 +13,7 @@ import { Operation } from "@/app/circuit-data/circuit-parsing";
  * @param qubitPositions The vertical arrangement of the qubits in the circuit
  * @returns [width: int, height: int]
  */
-const calculateGateDimensions = (operation: Operation, qubitPositions: Array<number>): [number, number] => {
+const calculateGateDimensions = (operation: Operation, qubitPositions: number[]): [number, number] => {
   
   const upperQubitPos = Math.min.apply(Math, operation.qubits.map(q => qubitPositions[q]));
   const lowerQubitPos = Math.max.apply(Math, operation.qubits.map(q => qubitPositions[q]));
@@ -31,6 +31,27 @@ const calculateGateDimensions = (operation: Operation, qubitPositions: Array<num
   ];
 }
 
+/**
+ * Find all qubit positions covered by the gate and its control points
+ * @param operation The quantum gate to calculate the dimensions of
+ * @param qubitOrder The qubit IDs arranged in vertical display order
+ * @param qubitPositions The vertical position of each qubit in the circuit
+ * @returns [width: int, height: int]
+ */
+const calculateOperationSpan = (operation: Operation, qubitOrder: number[], qubitPositions: number[]): [number, number, number[], number[]] => {
+  // Get a list of explicitly covered qubit positions (including controls)
+  const covered_positions = [...operation.qubits, ...operation.controls, ...operation.anticontrols].map((qubit) => qubitPositions[qubit]);
+  // Find the minimum & maximum covered qubit positions
+  const min_qubit_pos = Math.min(...covered_positions);
+  const max_qubit_pos = Math.max(...covered_positions);
+  // Create an array of qubit positions between the extremes
+  const covered_positions_filled = Array(max_qubit_pos - min_qubit_pos + 1).fill(0).map((_, j) => min_qubit_pos+j);
+  // Create an arrau of qubit IDs between the extremes
+  const covered_qubits_filled = covered_positions_filled.map((pos) => qubitOrder[pos]);
+  
+  return [min_qubit_pos, max_qubit_pos, covered_positions_filled, covered_qubits_filled];
+}
 
 
-export { calculateGateDimensions };
+
+export { calculateGateDimensions, calculateOperationSpan };
