31057888f4ed474b46bf4414b4b42a71f7a423ea
Anthony Wilson <awilsonprogramming@gmail.com>
2026-03-13 12:01:34 +1100

    Fix broken root links in markdown files
    - Root links in markdown files did not respect the NEXT_PUBLIC_BASEPATH
      by default. Adding a custom MDXComponent to override the 'a' tag
    should fix this.

--- quantum-computing-edu-next/mdx-components.tsx
+++ quantum-computing-edu-next/mdx-components.tsx
@@ -1,7 +1,12 @@
-import type { MDXComponents } from 'mdx/types'
+import type { MDXComponents } from 'mdx/types';
+import Link from "next/link";
  
-const components: MDXComponents = {}
- 
+const components: MDXComponents = {
+  a: ({ href, children }) => (
+    <Link href={href}>{children}</Link>
+  ),
+}
+
 export function useMDXComponents(): MDXComponents {
-  return components
+  return components;
 }
